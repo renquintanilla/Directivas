ca0df5825b9263774524fef058e58d872eef4ec4
renquintanilla <50958699+renquintanilla@users.noreply.github.com>
2022-08-16 21:46:03 -0600

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivas';
+
+  propiedadesParrafo: any;
+
+  constructor() {
+    this.propiedadesParrafo = { 
+      color: 'red', 
+      fontSize: '24px' 
+    }
+  }
 }
